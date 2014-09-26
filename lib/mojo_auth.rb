82da55d7ac723a76042009ff04b7263731c148ab
Ben Langfeld <ben@langfeld.me>
2014-09-25 23:13:07 -0300

    Implement creation of a secret

--- lib/mojo_auth.rb
+++ lib/mojo_auth.rb
@@ -1,3 +1,6 @@
+require 'base64'
+require 'digest'
+
 require 'mojo_auth/version'
 
 #
@@ -6,4 +9,10 @@ require 'mojo_auth/version'
 # MojoAuth is a set of standard approaches to cross-app authentication based on HMAC
 #
 module MojoAuth
+  # Create a new random secret
+  # @return [String] a new secret based on /dev/random
+  def self.create_secret
+    random = File.read('/dev/random', 512)
+    Base64.encode64(Digest::SHA2.new(512).digest(random))
+  end
 end
--- spec/mojo_auth_spec.rb
+++ spec/mojo_auth_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe MojoAuth do
+  describe 'create a secret' do
+    subject(:secret) { described_class.create_secret }
 
+    it { should be_a String }
+
+    it 'should be long enough' do
+      expect(secret.length).to be > 20
+    end
+  end
 end
